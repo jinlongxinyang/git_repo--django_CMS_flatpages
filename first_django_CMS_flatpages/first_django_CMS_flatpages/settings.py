a0e9b95295a585fddc5efc4d0af9790e3f2bab04
jinlongxinyang <jinlongxinyang@126.com>
2016-09-05 17:19:34 +0800

    init

--- first_django_CMS_flatpages/first_django_CMS_flatpages/settings.py
+++ first_django_CMS_flatpages/first_django_CMS_flatpages/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flatpages',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'first_django_CMS_flatpages.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_project_first_flatpages',
+        'HOST': 'localhost',
+	    'PORT':'3306',
+        'USER': 'root',
+        'PASSWORD': 'db1234',
     }
 }
 
